1167478eb512e6f13e4e7644e774103325470e0d
preciousekenenkwor <your.preciousekenenkwor@gmail.com>
2024-02-12 16:57:49 +0100

    update

---
+++ kat.py
@@ -0,0 +1,20 @@
+#Example lists
+print =['red','orange','blue','voilet','indig']
+
+#EXample strings
+print = ["hello"]
+
+#Example stings
+print = ["i love you"]
+
+#Example Diction
+print = {"USA":"Washington D.C.", "France":"Paris", "India":"New Delhi"}
+  
+  #Example TUPLE
+print = [1, 2,6,9,8 ]
+
+# EXample integars
+print = ((100))
+
+#float to integars 
+print = ((2.3))
